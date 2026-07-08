a2b61e4cd897eeed777b71b87c34e5a17b2faabd
mukeshcodes-dev <mukesh.creates.dev@gmail.com>
2026-07-09 01:23:00 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,34 @@
+const display = document.getElementById("display");
+
+function appendValue(value) {
+    display.value += value;
+}
+
+function clearDisplay() {
+    display.value = "";
+}
+
+function deleteLast() {
+    display.value = display.value.slice(0, -1);
+}
+
+function calculate() {
+
+    try {
+
+        if (display.value === "") {
+            return;
+        }
+
+        display.value = eval(display.value);
+
+    } catch (error) {
+
+        display.value = "Error";
+
+        setTimeout(() => {
+            display.value = "";
+        }, 1000);
+    }
+
+}
